56ac5c9ac6978123c361735aab3631de5bcd98e5
Oğuzhan KARAGÖL <oguzhankaragol60@gmail.com>
2016-12-05 11:23:27 +0300

    wall coordinates upgraded

--- src/bomberman/Board.java
+++ src/bomberman/Board.java
@@ -43,50 +43,50 @@ public class Board extends JPanel
         FragileWall fw;
         NonFragileWall nfw;
         int multiplier = 32;
+        int scorBoardSpace = 48;
+        int x;
+        int y;
 
         for (int line = 0; line < 15; line++)
         {
             for (int column = 0; column < 17; column++)
             {
+                x = column * multiplier;
+                y = line * multiplier + scorBoardSpace;
                 if ((line == 0) || (line == 14))
                 {
-                    nfw = new NonFragileWall(column * multiplier, line * multiplier);
+                    nfw = new NonFragileWall(x, y);
                     wall[line][column] = nfw;
-                    g.drawImage(nfw.getImage(), column * multiplier, line * multiplier, null);
-                } 
-                else if ((column % 2 == 0) && (line != 0) && (line != 14) && (line % 2 == 0))
+                    g.drawImage(nfw.getImage(), x, y, null);
+                } else if ((column % 2 == 0) && (line != 0) && (line != 14) && (line % 2 == 0))
                 {
-                    nfw = new NonFragileWall(column * multiplier, line * multiplier);
+                    nfw = new NonFragileWall(x, y);
                     wall[line][column] = nfw;
-                    g.drawImage(nfw.getImage(), column * multiplier, line * multiplier, null);
-                } 
-                else if ((column == 0) || (column == 16) && (line != 0) && (line != 14) && (line % 2 != 0))
+                    g.drawImage(nfw.getImage(), x, y, null);
+                } else if ((column == 0) || (column == 16) && (line != 0) && (line != 14) && (line % 2 != 0))
                 {
-                    nfw = new NonFragileWall(column * multiplier, line * multiplier);
+                    nfw = new NonFragileWall(x, y);
                     wall[line][column] = nfw;
-                    g.drawImage(nfw.getImage(), column * multiplier, line * multiplier, null);
-                } 
-                else if ((line == 1) && (column == 1) || (line == 1) && (column == 2) || (line == 1) && (column == 3) || (line == 2) && (column == 1) || (line == 3) && (column == 1))
+                    g.drawImage(nfw.getImage(), x, y, null);
+                } else if ((line == 1) && (column == 1) || (line == 1) && (column == 2) || (line == 1) && (column == 3) || (line == 2) && (column == 1) || (line == 3) && (column == 1))
                 {
-                } 
-                else if ((line == 13) && (column == 15) || (line == 13) && (column == 14) || (line == 13) && (column == 13) || (line == 12) && (column == 15) || (line == 11) && (column == 15))
+                } else if ((line == 13) && (column == 15) || (line == 13) && (column == 14) || (line == 13) && (column == 13) || (line == 12) && (column == 15) || (line == 11) && (column == 15))
                 {
-                } 
-                else
+                } else
                 {
-                    fw = new FragileWall(column * multiplier, line * multiplier);
+                    fw = new FragileWall(x, y);
                     wall[line][column] = fw;
-                    g.drawImage(fw.getImage(), column * multiplier, line * multiplier, null);
+                    g.drawImage(fw.getImage(), x, y, null);
                 }
                 if ((line == 1) && (column == 1))
                 {
-                    Players[0] = new Player(column * multiplier, line * multiplier);
-                    g.drawImage(Players[0].getImage(), column * multiplier, line * multiplier, null);
+                    Players[0] = new Player(x, y);
+                    g.drawImage(Players[0].getImage(), x, y, null);
                 }
                 if ((line == 13) && (column == 15))
                 {
-                    Players[1] = new Player(column * multiplier, line * multiplier);
-                    g.drawImage(Players[1].getImage(), column * multiplier, line * multiplier, null);
+                    Players[1] = new Player(x, y);
+                    g.drawImage(Players[1].getImage(), x, y, null);
                 }
             }
         }
